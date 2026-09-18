7e80d85b960fca3efd99e3fe13298495730078ad
zamuuk13-sketch <zamuuk13@gmail.com>
2026-09-18 06:36:18 -0300

    Integrate browser capability detection

--- src/core/index.js
+++ src/core/index.js
@@ -4,13 +4,17 @@
  */
 
 import { logger } from "./logger.js";
+import { getBrowserCapabilities } from "./capabilities.js";
 
 export function createCoreModule() {
   return {
     name: "core",
 
     initialize() {
+      const capabilities = getBrowserCapabilities();
+
       logger.info("Core module initialized.");
+      logger.debug("Browser capabilities detected.", capabilities);
     }
   };
 }
